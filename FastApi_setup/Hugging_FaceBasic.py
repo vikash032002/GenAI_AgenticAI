2b7eff7feb1a0348a27710a3714c2ce1ad98bb74
vikash032002 <vikuvikash03@gmail.com>
2026-04-17 00:17:28 +0530

    #9 using HuggingFaceModel

---
+++ FastApi_setup/Hugging_FaceBasic.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.getenv("HF_TOKEN"),
+)
+
+completion = client.chat.completions.create(
+    model="google/gemma-4-31B-it:fastest",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe this image in one sentence."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
+                    }
+                }
+            ]
+        }
+    ],
+)
+
+print(completion.choices[0].message)
